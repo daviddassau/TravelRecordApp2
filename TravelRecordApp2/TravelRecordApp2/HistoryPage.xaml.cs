a4b09b188cf0b902d68185ddb0341a3daf40d103
David Dassau <davidtdassau@gmail.com>
2019-05-27 09:38:24 -0500

    Read for the DB

--- TravelRecordApp2/TravelRecordApp2/HistoryPage.xaml.cs
+++ TravelRecordApp2/TravelRecordApp2/HistoryPage.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TravelRecordApp2.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +17,16 @@ namespace TravelRecordApp2
 		{
 			InitializeComponent ();
 		}
-	}
+
+        // Executes/Reads from DB every time user navigates to History page
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation);
+            conn.CreateTable<Post>();
+            var posts = conn.Table<Post>().ToList();
+            conn.Close();
+        }
+    }
 }
